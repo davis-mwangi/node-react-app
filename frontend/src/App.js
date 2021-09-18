d3bf476d5f5ab71d447986a40cdbbd313e4bcdf8
davidmwangi <daudimwash@gmail.com>
2021-09-19 00:56:49 +0300

    Add redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,51 +1,28 @@
 import React from 'react';
-import data from './data';
-
-
+import { Route, BrowserRouter } from 'react-router-dom';
+import ProductScreen from './screens/ProductScreen';
+import HomeScreen from './screens/HomeScreen';
 function App() {
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="index.html">amazona</a>
-        </div>
-        <div>
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
-        </div>
-      </header>
-      <main>
-        <div className="row center">
-          {data.products.map((product) => (
-            <div key ={product._id} className="card">
-              <a href={`/product/${product._id}`}>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="index.html">amazona</a>
+          </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Route path="/product/:id" component={ProductScreen} ></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
 
-                <img className="medium" src={product.image} alt="product" />
-              </a>
-              <div className="card-body">
-                <a href={`/product/${product._id}`}>
-                  <h2>{product.name}</h2>
-                </a>
-                <div className="rating">
-                  <span> <i className="fa fa-star"></i></span>
-                  <span> <i className="fa fa-star"></i></span>
-                  <span> <i className="fa fa-star"></i></span>
-                  <span> <i className="fa fa-star-half-o"></i></span>
-                  <span> <i className="fa fa-star-o"></i></span>
-
-                </div>
-                <div className="price">Ksh. {product.price}</div>
-              </div>
-
-            </div>
-          ))
-          }
-
-        </div>
-
-      </main>
-      <footer className="row center">All right reserved</footer>
-    </div>
+        </main>
+        <footer className="row center">All right reserved</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
