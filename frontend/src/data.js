da55e009fcada848cad0f21b14e97492d48f247e
davidmwangi <daudimwash@gmail.com>
2021-09-18 19:36:11 +0300

    Add Project files

---
+++ frontend/src/data.js
@@ -0,0 +1,73 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p1.png',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id: '2',
+            name: 'Adidas Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p2.jpeg',
+            price: 100,
+            brand: 'Adidas',
+            rating: 4.0,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id: '3',
+            name: 'Lacoste Fre Shirt',
+            category: 'Shirts',
+            image: '/images/p3.jpeg',
+            price: 220,
+            brand: 'Lacoste',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id: '4',
+            name: 'Nike Slim Pants',
+            category: 'Pants',
+            image: '/images/p4.jpeg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 18,
+            description: 'high quality product'
+        },
+        {
+            _id: '5',
+            name: 'Puma Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p5.jpeg',
+            price: 120,
+            brand: 'Puma',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id: '6',
+            name: 'Adidad Slim Pants',
+            category: 'Shirts',
+            image: '/images/p6.jpeg',
+            price: 120,
+            brand: 'Pants',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+
+    ],
+};
+
+export default data;
